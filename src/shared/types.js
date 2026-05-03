c9be3fb25e035c3b7c90bed1f885c44979c6b23f
Aanish Bhirud <aanishbhirud@gmail.com>
2026-05-03 19:50:09 -0400

    fix: use esbuild to bundle renderer, single tsconfig for CJS, add bundle.js to HTML

---
+++ src/shared/types.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DEFAULT_CONFIG = void 0;
+exports.DEFAULT_CONFIG = {
+    selectedPetId: null,
+    position: { x: 0, y: 0 },
+    alwaysOnTop: true,
+    pollIntervalMs: 30000
+};
+//# sourceMappingURL=types.js.map
